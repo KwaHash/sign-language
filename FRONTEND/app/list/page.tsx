a192b62a954dfe5491ba054de0b27a4597cf1a98
StackNinja0109 <whitecrow0109@gmail.com>
2025-01-08 06:18:03 +0900

    fix: from server component to client one for list page

--- FRONTEND/app/list/page.tsx
+++ FRONTEND/app/list/page.tsx
@@ -1,24 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import axios from "axios";
 import ListPage from "@/features/ListPage";
 import type { Video } from "@/utils/types";
 
 export default async function Page() {
-  try {
-    // Fetch data server-side during rendering
-    const res = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/list`);
-    const videos = res.data as Video[];
+  const [videos, setVideos] = useState<Video[]>([]);
+  useEffect(() => {
+    const fetchSigns = async () => {
+      const res = await axios.post('/api/list');
+      if (res.status === 200) {
+        setVideos(res.data as Video[]);
+      }
+    }
+    fetchSigns();
+  }, [])
 
-    return (
-      <main className="flex min-h-screen grow ml-[250px]">
-        <ListPage videos={videos} />
-      </main>
-    );
-  } catch (error) {
-    console.error("Error fetching videos:", error);
-    return (
-      <main className="flex min-h-screen grow ml-[250px]">
-        <ListPage videos={[]} /> {/* Empty list in case of error */}
-      </main>
-    );
-  }
+  return (
+    <main className="flex min-h-screen grow ml-[250px]">
+      <ListPage videos={videos} />
+    </main>
+  );
 }
